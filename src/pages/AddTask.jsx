ad2b555f2f19948e691e60939cf0bc9cb7e11514
Mattia Natale <mattianatale07@gmail.com>
2025-06-16 18:00:39 +0200

    feat: add milestone 5

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,7 +1,64 @@
+import { useState, useRef, useMemo } from "react";
+
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\\,.<>?/`~";
+
 export default function AddTask() {
+  const [taskTitle, setTaskTitle] = useState("");
+  const descriptionRef = useRef();
+  const statusRef = useRef();
+
+  const taskTitleError = useMemo(() => {
+    if (!taskTitle.trim()) return "Il nome della task non può essere vuoto";
+
+    if ([...taskTitle].some((char) => symbols.includes(char)))
+      return "Il nome della task non può contenere simboli";
+
+    return "";
+  }, [taskTitle]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (taskTitleError) return;
+
+    const newTask = {
+      title: taskTitle.trim(),
+      description: descriptionRef.current.value,
+      status: statusRef.current.value,
+    };
+    console.log(newTask);
+  };
+
   return (
-    <div>
+    <main className="add-task-page">
       <h1>Aggiungi una task</h1>
-    </div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nome task:
+          <input
+            type="text"
+            value={taskTitle}
+            onChange={(e) => setTaskTitle(e.target.value)}
+          />
+        </label>
+        {taskTitleError && <p style={{ color: "red" }}>{taskTitleError}</p>}
+        <label>
+          Descrizione:
+          <textarea ref={descriptionRef}></textarea>
+        </label>
+        <label>
+          Stato:
+          <select ref={statusRef} defaultValue={"To do"}>
+            {["To do", "Doing", "Done"].map((value, index) => (
+              <option key={index} value={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button type="submit" disabled={taskTitleError}>
+          Aggiungi task
+        </button>
+      </form>
+    </main>
   );
 }
